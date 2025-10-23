3a871f5b959167a8d7ca3e6131ce8d9c1094bffc
Nikolai40Kuznetsov <nikolaikuznetsov@tutamail.com>
2025-10-23 13:24:37 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
+import pandas as pd
+import datetime
+import uuid
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
@@ -19,3 +22,16 @@ def get_start_page(request: Request):
 @app.get("/main", response_class=HTMLResponse)
 def get_start_page(request: Request):
     return templates.TemplateResponse("main.html", {"request": request})
+
+@app.get("/", response_class=HTMLResponse)
+@app.get("/login", response_class=HTMLResponse)
+def get_login_page(request: Request):
+    return templates.TemplateResponse("login.html", {"request": request})
+
+@app.post("/login")
+def login(request: Request, username: str = Form(...), password: str = Form(...)):
+    users = pd.read_csv("users.csv")
+    if username in users['user'].values[0]:
+        if str(users[users["user"] == username].values[0][1]) == password:
+            session_id = str(uuid.uuid4())
+            sessions[session_id] = datetime.now()
