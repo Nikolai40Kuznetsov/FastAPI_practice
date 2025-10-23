879f49b9e753ff9d2ac84f0d46ebb7132742ec89
Nikolai40Kuznetsov <nikolaikuznetsov@tutamail.com>
2025-10-23 13:12:32 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+app.mount("/sources", StaticFiles(directory="sources"), name="sources")
 templates = Jinja2Templates(directory="templates")
+USERS = "users.csv"
+# SESSION_TTL = timedelta(10)
+sessions = {}
+white_urls = ["/", "/login", "/logout"]
 
 @app.get("/", response_class=HTMLResponse)
 def get_start_page(request: Request):
