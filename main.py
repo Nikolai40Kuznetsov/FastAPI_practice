eddbeeae8ca7df40df959ca8e8675901d862bfb6
Nikolai40Kuznetsov <nikolaikuznetsov@tutamail.com>
2025-10-24 15:50:53 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-import csv, os, datetime, uuid
+import csv, os, uuid
 import pandas as pd
+from datetime import datetime, timedelta
 
 
 app = FastAPI()
@@ -12,10 +13,25 @@ app.mount('/templates', StaticFiles(directory='templates'), name='templates')
 app.mount('/sources', StaticFiles(directory='sources'), name='sources')
 templates = Jinja2Templates(directory = "templates")
 USERS = 'users.csv'
+SESSION_TTL = timedelta(10)
+sessions = {}
+white_urls = ["/", "/login", "/logout"]
 
-# @app.get("/", response_class=HTMLResponse)
-# def get_start_page(request: Request):
-#     return templates.TemplateResponse("main.html", {"request": request})
+# session_id = uuid.uuid4()
+# session_id_str = str(session_id)
+# print(session_id_str)
+
+# @app.middleware("http")
+# async def check_session(request: Request, call_next):
+#     if request.url.path.startswith("/static") or request.url.path in white_urls:
+#         return await call_next(request)
+    
+    # session_id = request.cookies.get("session_id")
+    # if session_id not in sessions:
+
+
+    
+
 
 @app.get("/", response_class=HTMLResponse)
 @app.get("/login", response_class=HTMLResponse)
@@ -29,16 +45,20 @@ def login(request: Request,
     users = pd.read_csv(USERS)
     if username in users['user'].values[0]:
         if str(users[users["user"] == username].values[0][1]) == password:
-            #session_id = str(uuid.uuid4())
-            #session[session_id] = datetime.now()
+            session_id = str(uuid.uuid4())
+            sessions[session_id] = datetime.now()
             response = RedirectResponse(url=f"/main/{username}", status_code=302)
-            #response.set_cookie(key='session_id', value=session_id)
+            response.set_cookie(key='session_id', value=session_id)
             return response
         return templates.TemplateResponse("login.html",
                                           {'request':request,
-                                           'error': 'Неверный логин или пароль'})
+                                           'error': 'Неверный пароль'})
+    return templates.TemplateResponse("login.html",
+                                          {'request':request,
+                                           'error': 'Неверный логин'})
     
 @app.get("/main/admin", response_class=HTMLResponse)
 def get_start_page(request:Request):
     return templates.TemplateResponse("main.html", {'request': request})
+
 
